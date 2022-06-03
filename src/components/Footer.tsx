9daecafaa300faebbd9c3d2c10353cdd90cf5071
Henrique Zanini <hiquebzanini@gmail.com>
2022-06-03 20:56:49 -0300

    criando header e footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>
+          <span>Todo React + Typescript</span> @ 2022
+        </p>
+    </footer>
+  )
+}
+
+export default Footer
